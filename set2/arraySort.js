f40532d1d35143dc5533e83e95cc95d69dba6bea
Reellz <atwanbwambs@gmail.com>
2025-09-02 08:57:26 +0300

    Complete arrraySort challenge.

--- set2/arraySort.js
+++ set2/arraySort.js
@@ -9,9 +9,10 @@ Caution: DO NOT use the sort array method */
 1. Define a function that takes an array as a parameter.
 2. Initialize three empty lists.
 3. Loop through each element in the array.
-4. Sort each list 
-5. Create an object with three keys
-6. Return the object. */
+4. Define a manualSort function that takes a list as input
+5. Sort each list 
+6. Create an object with three keys
+7. Return the object. */
 
 function arraySort(someArray) {
   const evens = [];
@@ -19,20 +20,39 @@ function arraySort(someArray) {
   const chars = [];
 
   for (let i = 0; i < someArray.length; i++) {
-    if (typeof someArray[i] === 'number'){
+    if (typeof someArray[i] === 'number') {
       if (someArray[i] % 2 === 0) {
-        evens.push(someArray[i])
-      } else{
-        odds.push(someArray[i])
+        evens.push(someArray[i]);
+      } else {
+        odds.push(someArray[i]);
       }
-    }else if (typeof someArray[i] === 'string' && someArray[i].length === 1){
-      chars.push(someArray[i])
+    } else if (typeof someArray[i] === 'string' && someArray[i].length === 1) {
+      chars.push(someArray[i]);
     }
   }
 
-  return {evens, odds, chars}
+  return {
+    evens: manualSort(evens),
+    odds: manualSort(odds),
+    chars: manualSort(chars)
+  };
+}
+
+function manualSort(array) {
+  const n = array.length;
+
+  for (let i = 0; i < n - 1; i++) {
+    for (let j = 0; j < n - i - 1; j++) {
+      if (array[j] > array[j + 1]) {
+        let temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+      }
+    }
+  }
+  return array;
 }
 
 // Test arraySort
-let someArray = [3.0, 'a', 7, 'x', '20', 'd', 4, 'f', 8]
-console.log(arraySort(someArray))
+console.log(arraySort([3.0, 'a', 7, 'x', '20', 'd', 4, 'f', 8])); 
+
